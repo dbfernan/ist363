95c3a0485056af2a7e069dbaa27d947b9e855476
dbfernan <dbfernan@syr.edu>
2026-02-23 17:33:20 -0500

    project1 final

---
+++ projects/mantis-shrimp/script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+    M.Tabs.init(document.querySelectorAll('.tabs'));
+    M.Collapsible.init(document.querySelectorAll('.collapsible'));
+    M.ScrollSpy.init(document.querySelectorAll('.scrollspy'));
+    M.Sidenav.init(document.querySelectorAll('.sidenav'));
+    M.Parallax.init(document.querySelectorAll('.parallax'));
+    M.Materialbox.init(document.querySelectorAll('.materialboxed'));
+  
+    setTimeout(function () {
+      const loader = document.getElementById('loader');
+      if (loader) loader.style.display = "none";
+    }, 800);
+  });
