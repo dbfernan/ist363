751641c292fbbc130f759e1d4b5c9dc2ad490cbf
dbfernan <dbfernan@syr.edu>
2026-02-23 18:01:43 -0500

    folder adjustments

---
+++ week7/participation7.js
@@ -0,0 +1,11 @@
+let gpa = 3.4;
+
+function determineLetterGrade(gpa) {
+    if (gpa >= 3.5) {
+    console.log("You are on the Dean's List!");
+    } 
+    else {
+    console.log("You did not make the Dean's List.");
+    }
+}
+
